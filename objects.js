ec2b19b9de9fadddabad28be43b32aea9349dc2d
Thomas Barnes <barnes.thomas.c@gmail.com>
2018-05-22 20:32:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,8 +5,8 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 }
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist;
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
   return playlist;
 }
 
